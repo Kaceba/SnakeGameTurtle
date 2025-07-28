0c2f5dd162abb65524eb90890b89faf5cbb58146
Roberto Vincenzo Ninno <RobertoNinno23@outlook.it>
2025-07-28 17:27:55 +0200

    added food logic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +10,7 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -21,5 +23,10 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    #Detect collision with food
+    if snake.head.distance(food) < 20:
+        food.refresh()
+
 
 screen.exitonclick()
