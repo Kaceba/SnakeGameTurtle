141b27b5351a01965311e83f1a48618dbfd141f1
Roberto Vincenzo Ninno <RobertoNinno23@outlook.it>
2025-07-29 17:11:30 +0200

    stopped resize from user

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.getcanvas().winfo_toplevel().resizable(False, False)
 screen.tracer(0)
 
 snake = Snake()
